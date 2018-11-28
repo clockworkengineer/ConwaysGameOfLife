754d2a508b0a40b265895ce7e021369c3f250f58
Rob Tizzard <robert_tizzard@hotmail.com>
2018-11-28 17:12:08 +0000

    Introduction of CLife class and usage.

--- CLife.cpp
+++ CLife.cpp
@@ -9,10 +9,108 @@
 #include <cstring>
 #include "CLife.hpp"
 
-CLife::CLife(int cellGridWidth, int cellGridHeight) : m_cellGridWidth{cellGridWidth}, m_cellGridHeight{cellGridHeight} 
+CLife::CLife(int cellGridHeight, int cellGridWidth) : m_cellGridHeight{cellGridHeight}, m_cellGridWidth{cellGridWidth}
 {
-    m_cellGrid.reset(new std::uint8_t [m_cellGridWidth*m_cellGridHeight]);
-    m_cellGridCopy.reset(new std::uint8_t [m_cellGridWidth*m_cellGridHeight]);
+    m_cellMasterGrid.reset(new std::uint8_t [m_cellGridWidth * m_cellGridHeight]);
+    m_cellGridReadOnly.reset(new std::uint8_t [m_cellGridWidth * m_cellGridHeight]);
+}
+
+CLife::~CLife() {
+}
+
+std::pair<int, int> CLife::gridBounds(int y, int x) {
+    
+    if (y < 0) {
+        y = m_cellGridHeight - 1;
+    } else if (y == m_cellGridHeight) {
+        y = 0;
+    }
+
+    if (x < 0) {
+        x = m_cellGridWidth - 1;
+    } else if (x == m_cellGridWidth) {
+        x = 0;
+    }
+    
+    return(std::make_pair(y, x));
+    
+}
+
+bool CLife::isCellActive(int y, int x) {
+
+    auto coords = gridBounds(y, x);
+
+    return (m_cellGridReadOnly[cellIndex(coords.first, coords.second)]);
+
+}
+
+int CLife::activeCellNeighbours(int y, int x) {
+    
+    int activeNeighbours = 0;
+    
+    if (isCellActive(y - 1, x - 1)) {
+        activeNeighbours++;
+    }
+    if (isCellActive(y - 1, x)) {
+        activeNeighbours++;
+    }
+    if (isCellActive(y - 1, x + 1)) {
+        activeNeighbours++;
+    }
+    if (isCellActive(y, x - 1)) { 
+        activeNeighbours++;
+    }
+    if (isCellActive(y, x + 1)) { 
+        activeNeighbours++;
+    }
+    if (isCellActive(y + 1, x - 1)) {
+        activeNeighbours++;
+    }
+    if (isCellActive(y + 1, x)) {
+        activeNeighbours++;
+    }
+    if (isCellActive(y + 1, x + 1)) {
+        activeNeighbours++;
+    }
+    
+    return (activeNeighbours);
+
+}
+
+void CLife::update() {
+
+    std::memcpy(m_cellGridReadOnly.get(), m_cellMasterGrid.get(), this->m_cellGridHeight * this->m_cellGridWidth);
+
+    for (int y = 0; y < this->m_cellGridHeight; y++) {
+        for (int x = 0; x < this->m_cellGridWidth; x++) {
+            int activeCellCount = activeCellNeighbours(y, x);
+            if (isCellActive(y, x)) {
+                if ((activeCellCount < 2) || (activeCellCount > 3)) {
+                    setCell(y, x, false);
+                }
+            } else {
+                if (activeCellCount == 3) {
+                    setCell(y, x, true);
+                }
+            }
+        }
+    }
+}
+
+void CLife::setCell(int y, int x, bool active) {
+
+    auto coords = gridBounds(y, x);
+
+    m_cellMasterGrid[cellIndex(coords.first, coords.second)] = active;
+
+}
+
+bool CLife::getCell(int y, int x) {
+
+    auto coords = gridBounds(y, x);
+       
+    return (m_cellMasterGrid[cellIndex(coords.first, coords.second)]);
+    
 }
 
 int CLife::getCellGridHeight() const {
@@ -21,75 +119,4 @@ int CLife::getCellGridHeight() const {
 
 int CLife::getCellGridWidth() const {
     return m_cellGridWidth;
-}
-
-CLife::~CLife() {
-}
-
-int CLife::cellIndex(int y, int x) {
-    return(y*this->m_cellGridHeight+x);
-}
-
-bool CLife::cellActive(int y, int x) {
-
-    if (y < 0) {
-        y = m_cellGridHeight-1;
-    } else if (y == m_cellGridHeight) {
-        y = 0;
-    }
-    
-    if (x < 0) { 
-        x = m_cellGridWidth-1;
-    } else if (x == m_cellGridWidth) {
-        x = 0;
-    }
-    
-    return(m_cellGridCopy[cellIndex(y, x)]==kCellActive);
-     
-}
-
-int CLife::activeCellNeighbours(int y, int x) {
-    int active=0;
-    if (cellActive(y-1, x-1))active++;
-    if (cellActive(y-1,  x)) active++;
-    if (cellActive(y-1, x+1)) active++;
-    if (cellActive(y, x-1)) active++;
-    if (cellActive(y, x+1)) active++;
-    if (cellActive(y+1, x-1)) active++;
-    if (cellActive(y+1, x)) active++;
-    if (cellActive(y+1, x+1)) active++;
-    return(active);
-    
-}
-
-void CLife::update() {
-    
-    std::memcpy(m_cellGridCopy.get(), m_cellGrid.get(), this->m_cellGridHeight*this->m_cellGridWidth);
-    
-    for (int y = 0; y < this->m_cellGridHeight; y++) {
-        for (int x = 0; x < this->m_cellGridWidth; x++) {
-            int active = activeCellNeighbours(y, x);
-            if (m_cellGridCopy[cellIndex(y, x)]==kCellActive) {
-                if ((active < 2) || (active > 3)){
-                    m_cellGrid[cellIndex(y, x)]=kCellDead;
-                } 
-            } else {
-                if (active==3) {
-                    m_cellGrid[cellIndex(y, x)]=kCellActive;
-                }
-            }
-        }
-    }
-}
-
-void CLife::setCell(int y, int x, bool active) {
-    if (active) {
-         m_cellGrid[cellIndex(y, x)]=kCellActive;
-    } else{
-        m_cellGrid[cellIndex(y, x)]=kCellDead;
-    }
-}
-
-bool CLife::getCell(int y, int x) {
-    return(m_cellGrid[cellIndex(y, x)]==kCellActive);
 }
